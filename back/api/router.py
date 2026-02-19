08ab9d5e81a529e06c94d7bbcef71a1049d1f1cd
AlvaroCaba <alvaro_caba2009@uma.es>
2026-02-19 13:13:41 +0100

    Remove scenarios module and its router from API

--- back/api/router.py
+++ back/api/router.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter
 from back.api.routes.database import router as database_router
 from back.api.routes.SARIMAX_model import router as sarimax_router
 from back.api.routes.XGBoost_model import router as xgboost_router
-from back.api.routes.scenarios import router as scenarios_router
 from back.config import settings
 
 
@@ -11,4 +10,3 @@ api_router = APIRouter(prefix=settings.get("server.api_prefix", "/api"))
 api_router.include_router(database_router)
 api_router.include_router(sarimax_router)
 api_router.include_router(xgboost_router)
-api_router.include_router(scenarios_router)
